f0e2b4fcf150128160ad50a870202a75343ea27e
Prinshu <672024@cognizant.com>
2020-06-11 15:48:36 +0530

    sort of final

--- src/Mylist/Mylist.js
+++ src/Mylist/Mylist.js
@@ -10,7 +10,7 @@ const Mylist = props => {
     else {
         let count = 0;
         for (let i = 0; i < list.length; i++) {
-            if (list[i] === props.addElement) {
+            if (list[i].id === props.addElement.id) {
                 count++;
             }
         }
@@ -18,33 +18,12 @@ const Mylist = props => {
             list.push(props.addElement);
         }
     }
-    const deleteHandler = (id) => {
-        // console.log(list);
-        // console.log("index"+index);
-        // const tempList=[...list];
-        // tempList.splice(index,1);
-        // setList(tempList);
-        // console.log("tempList"+list)
-    //     // let tempArr = list.filter(item1 => item1 !== item);
-    //     // list = [...tempArr];
-    //     // console.log(list);
-    //     console.log(list)
-    //     console.log(item + "   dwqehwrjeb")
-        // setList(prevList =>
-        //     prevList.filter(listItem => listItem.id !== id));
-        console.log("id: "+id)
-        console.log("prev: "+list)
-        setList(list=>list.filter(item => item.id !== id));
-        console.log(list)
-     }
     return (
         <div className={classes.Mylist}>
             <h4>MY LIST</h4>
             <ul>
                 {list.map((listItem,index) => (
-                    <li key={listItem.id} 
-                     onClick={() => deleteHandler(listItem.id)}
-                >{listItem.name}</li>
+                    <li key={listItem.id} >{listItem.name}</li>
                 ))}
             </ul>
         </div>
